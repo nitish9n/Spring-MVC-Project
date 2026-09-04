06f68a2f8a6c4d976711d903236f17a65f3a6b1e
aarav <aaravaryan24@gmail.com>
2026-09-04 20:55:28 +0530

    update

--- src/main/java/learning/spring/mvc/configuration/MyWebServlet.java
+++ src/main/java/learning/spring/mvc/configuration/MyWebServlet.java
@@ -1,15 +1,24 @@
 package learning.spring.mvc.configuration;
 
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRegistration;
 
 public class MyWebServlet implements WebApplicationInitializer{
 
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
-		// TODO Auto-generated method stub
+		AnnotationConfigWebApplicationContext annWebConfig = new AnnotationConfigWebApplicationContext();
+		annWebConfig.register(SpringConfig.class);
+		annWebConfig.setServletContext(servletContext);
+		ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(annWebConfig));
+		servlet.setLoadOnStartup(1);
+		servlet.addMapping("/");
+		
 		
 	}
 	

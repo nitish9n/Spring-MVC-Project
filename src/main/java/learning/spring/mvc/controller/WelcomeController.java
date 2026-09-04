8d9efbc6de79b246ee32db50c6e36e2c983000db
aarav <aaravaryan24@gmail.com>
2026-09-05 01:23:58 +0530

    update

--- src/main/java/learning/spring/mvc/controller/WelcomeController.java
+++ src/main/java/learning/spring/mvc/controller/WelcomeController.java
@@ -1,7 +1,9 @@
 package learning.spring.mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WelcomeController {
@@ -26,6 +28,20 @@ public class WelcomeController {
 	public String redirect() {
 		System.out.println("redirect to admin");
 		return "redirect:/admin";        // it will redirect to the admin page
+	}
+	
+	
+	// Request param
+	@RequestMapping("/book")
+	public String requestParam(@RequestParam("bookName") String bookName,
+			@RequestParam("price") int price, 
+			Model model) {
+		model.addAttribute("myModel", bookName);
+		model.addAttribute("mulya", price);
+		
+		return "myView";
+		
+		// http://localhost:8080/Spring-MVC-Project/book?bookName=java
 	}
 	
 

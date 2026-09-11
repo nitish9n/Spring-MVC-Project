cf80363c800b6c57796b7e6f5d489f658f804663
aarav <aaravaryan24@gmail.com>
2026-09-11 19:45:19 +0530

    update

--- src/main/java/learning/spring/mvc/configuration/SpringConfig.java
+++ src/main/java/learning/spring/mvc/configuration/SpringConfig.java
@@ -1,8 +1,17 @@
 package learning.spring.mvc.configuration;
 
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jca.support.LocalConnectionFactoryBean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.hibernate.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.hibernate.HibernateTransactionManager;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -18,7 +27,6 @@ public class SpringConfig implements WebMvcConfigurer{
 	@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-		// jstlView is a class provided by spring framework, specially designed to handle jsp pages using jstl
 		viewResolver.setViewClass(JstlView.class);
 		viewResolver.setPrefix("/WEB-INF/VIEWS/");
 		viewResolver.setSuffix(".jsp");
@@ -26,5 +34,41 @@ public class SpringConfig implements WebMvcConfigurer{
 		return viewResolver;
 		
 	}
+	
+	@Bean
+	public DataSource dataSource() {
+		
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/mydb");
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+		return dataSource;
+		
+	}
+	
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+		
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource());
+		sessionFactory.setPackagesToScan("learning.spring.mvc");
+		Properties hibernateProperties = new Properties();
+		hibernateProperties.put("hibernate.dialect","org.hibernate.dialect.MySQLDialect");
+		hibernateProperties.put("hibernate.show_sql","true");
+		hibernateProperties.put("hibernate.hbm2ddl.auto","create");
+		sessionFactory.setHibernateProperties(hibernateProperties);
+		return sessionFactory;
+		
+	}
+	
+	@Bean
+	public HibernateTransactionManager transactionManager(
+	        SessionFactory sessionFactory) {
+
+	    return new HibernateTransactionManager(sessionFactory);
+	}
+	
 
 }
+
